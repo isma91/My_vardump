cd4621ab4cad0ad7e5598aac578687381856e71a
isma91 <ismaydogmus@gmail.com>
2016-05-23 22:10:55 +0200

    adding space for array and display array

--- index.php
+++ index.php
@@ -14,45 +14,47 @@
 /*
 *@TODO: check ressource ?
 */
-function display_null () {
-	echo "NULL\n";
+function display_null ($space = 0) {
+	echo str_repeat(" ", $space) . "NULL\n";
 }
-function display_boolean ($value) {
+function display_boolean ($value, $space = 0) {
 	if ($value === false) {
-		echo "bool(false)" . "\n";
+		echo str_repeat(" ", $space) . "bool(false)" . "\n";
 	} else {
-		echo "bool(true)" . "\n";
+		echo str_repeat(" ", $space) . "bool(true)" . "\n";
 	}
 }
-function display_string ($value) {
-	echo 'string(' . strlen($value) . ') "' . $value . '"' . "\n";
+function display_string ($value, $space = 0) {
+	echo str_repeat(" ", $space) . 'string(' . strlen($value) . ') "' . $value . '"' . "\n";
 }
-function display_int ($value) {
-	echo "int(" . $value . ")\n";
+function display_int ($value, $space = 0) {
+	echo str_repeat(" ", $space) . "int(" . $value . ")\n";
 }
-function display_double ($value) {
-	echo "double(" . $value . ")\n";
+function display_double ($value, $space = 0) {
+	echo str_repeat(" ", $space) . "double(" . $value . ")\n";
 }
-function display_array ($value) {
-	$space_repeat_count = 2;
-	echo "array(" . count($value) . ") {\n";
+function display_array ($value, $space = 0) {
+	echo str_repeat(" ", $space) . "array(" . count($value) . ") {\n";
+	$space = $space + 2;
 	foreach ($value as $value_key => $value_value) {
-		echo str_repeat(" ", $space_repeat_count) . "'" . $value_key . "' =>\n";
+		echo str_repeat(" ", $space) . "'" . $value_key . "' =>\n";
+		my_vardump($value_value, $space);
 	}
+	echo str_repeat(" ", $space - 2) . "}\n";
 }
-function my_vardump ($value) {
+function my_vardump ($value, $space = 0) {
 	if (is_array($value)) {
-		display_array($value);
+		display_array($value, $space);
 	} elseif (is_bool($value)) {
-		display_boolean($value);
+		display_boolean($value, $space);
 	} elseif (is_float($value) || is_double($value)) {
-		display_double($value);
+		display_double($value, $space);
 	} elseif (is_int($value)) {
-		display_int($value);
+		display_int($value, $space);
 	} elseif (is_null($value)) {
-		display_null();
+		display_null($space, $space);
 	} elseif (is_object($value)) {
 	} elseif (is_string($value)) {
-		display_string($value);
+		display_string($value, $space);
 	}
 }
